af1321fd51ccbad63cfd813e3883cf6bb47188a1
MrBoombastic <contact@amroz.xyz>
2026-09-06 22:07:17 +0200

    chore: remove unused eslint plugin and husky configuration

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -8,42 +8,34 @@ const tsParser = require("@typescript-eslint/parser");
 const typescriptEslint = require("@typescript-eslint/eslint-plugin");
 const js = require("@eslint/js");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+module.exports = defineConfig([
+    js.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+            parserOptions: {},
         },
 
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-        parserOptions: {},
-    },
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
 
-    extends: compat.extends("eslint:recommended"),
-
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            semi: "off",
+            quotes: ["error", "double"],
+            "no-undef": "off",
+            "no-unused-vars": "off",
+        },
     },
-
-    rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
-        semi: "off",
-        quotes: ["error", "double"],
-        "no-undef": "off",
-        "no-unused-vars": "off",
-    },
-}, globalIgnores(["**/dist/", "**/node_modules/", "**/*.js"])]);
+    globalIgnores(["**/dist/", "**/node_modules/", "**/*.js"]),
+]);
